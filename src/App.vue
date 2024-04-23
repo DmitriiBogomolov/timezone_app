b3bdd3d37c024d49a48cd6764077a2ac52ecdf22
DmitriiBogomolov <foreo1997@gmail.com>
2024-04-23 16:25:05 +1000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-main>
+      <AppBody />
+
+
+    </v-main>
+    <AppFooter />
+  </v-app>
+</template>
+
+<script setup>
+  //
+</script>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-footer height="40" app>
+    <a
+      v-for="item in items"
+      :key="item.title"
+      :href="item.href"
+      :title="item.title"
+      class="d-inline-block mx-2 social-link"
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      <v-icon
+        :icon="item.icon"
+        :size="item.icon === '$vuetify' ? 24 : 16"
+      />
+    </a>
+
+    <div
+      class="text-caption text-disabled"
+      style="position: absolute; right: 16px;"
+    >
+      &copy; {{ (new Date()).getFullYear() }} <span class="d-none d-sm-inline-block">Dmitrii Bogomolov</span>
+      —
+      MIT License
+
+    </div>
+  </v-footer>
+</template>
+
+<script setup>
+  const items = [
+    {
+      title: 'About author',
+      icon: `mdi-github`,
+      href: 'https://github.com/DmitriiBogomolov',
+    },
+  ]
+</script>
+
+<style scoped lang="sass">
+  .social-link :deep(.v-icon)
+    color: rgba(var(--v-theme-on-background), var(--v-disabled-opacity))
+    text-decoration: none
+    transition: .2s ease-in-out
+
+    &:hover
+      color: rgba(25, 118, 210, 1)
+</style>
